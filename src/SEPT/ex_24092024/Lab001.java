5eed97a9fe90076d69f7f2dd1a7c83b8e92206f1
PALAKTULI777 <PALAKTULI10@GMAIL.COM>
2024-11-07 04:57:45 +0530

    all the files

--- src/SEPT/ex_24092024/Lab001.java
+++ src/SEPT/ex_24092024/Lab001.java
@@ -1,4 +1,7 @@
 package SEPT.ex_24092024;
 
 public class Lab001 {
+    public static void main(String[] args) {
+
+    }
 }
--- src/SEPT/ex_25092024/TASK.java
+++ src/SEPT/ex_25092024/TASK.java
@@ -1,4 +1,8 @@
 package SEPT.ex_25092024;
 
 public class TASK {
+public static void main(String[] args){
+    System.out.println("hello");
+    System.out.println(2+2);
+}
 }
